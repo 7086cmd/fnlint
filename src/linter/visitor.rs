adc4eb31a04e6df39c3328bc17e61632ff19b469
Ethan Goh <7086cmd@gmail.com>
2024-08-23 13:37:34 +0800

    chore: format.

--- src/linter/visitor.rs
+++ src/linter/visitor.rs
@@ -5,7 +5,8 @@ use std::sync::Arc;
 pub fn lint_filenames(config: &Arc<FilenameLintConfig>, file_list: Vec<String>) -> Vec<Issue> {
   let mut result = vec![];
   config.ls.iter().for_each(|(ext, patterns)| {
-    let patterns: Arc<Vec<FilenameCase>> = Arc::new(patterns.into_iter().map(|case| *case).collect());
+    let patterns: Arc<Vec<FilenameCase>> =
+      Arc::new(patterns.into_iter().map(|case| *case).collect());
     let files = file_list.iter().filter(|file| file.ends_with(ext)).cloned().collect();
     let issues = lint_files(files, ext.to_string(), &patterns);
     issues.into_iter().for_each(|issue| result.push(issue));
@@ -29,7 +30,11 @@ mod tests {
       },
       ignore: vec![],
     });
-    let files = vec!["src/main.rs".to_string(), "src/linter/mod.rs".to_string(), "src/linter/hello-world.rs".to_string()];
+    let files = vec![
+      "src/main.rs".to_string(),
+      "src/linter/mod.rs".to_string(),
+      "src/linter/hello-world.rs".to_string(),
+    ];
     let issues = lint_filenames(&config, files);
     assert_eq!(issues.len(), 1);
   }
